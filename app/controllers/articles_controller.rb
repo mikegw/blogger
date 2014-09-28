ce400c2e73e1e9f1ebf8a04bad5606e685bc3af5
App Academy Student <student@appacademy.io>
2014-09-28 14:01:42 -0400

    finish blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
+
   end
 
   def create
